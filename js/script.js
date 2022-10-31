296bdfdfef0c5c56f3d02120e28d6532e56e2c4e
pavankumarkorrapati <korrapatipavankumar2000@gmail.com>
2022-10-31 01:16:51 -0400

    javascript changes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const Register = document.getElementById('register');
+
+if(Register) Register.addEventListener('submit', displayname);
+
+function displayname(e){
+
+let Fname = document.getElementById("fname").value;
+let Lname = document.getElementById("lname").value;
+let Username = document.getElementById("username").value;
+let Password = document.getElementById("pass").value;
+console.log("first name:"+ Fname);
+console.log("last name:"+ Lname);
+console.log("username:"+ Username);
+console.log("password:"+ Password);
+}
